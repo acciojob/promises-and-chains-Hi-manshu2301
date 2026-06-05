20d970ba3637c3f5ed18d88d07109c5723dc73df
Hi-manshu2301 <104667314+Hi-manshu2301@users.noreply.github.com>
2026-06-05 10:23:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+document.getElementById("form").addEventListener("submit",function(e){
+	e.preventDefault();
+	let name = document.getElementById("name");
+	let age = document.getElementById("age");
+	if(name == "" || age == ""){
+		alert("Please enter valid details.");
+		return;
+	}
+	let promise = new Promise(function(resolve,reject){
+		setTimeout(()=>{
+			if(age>18){
+				resolve(name);
+			}else{
+				reject(name);
+			}
+		},4000);
+	});
+	Promise.then(function (username) {
+		alert(`Welcome, ${username}. You can vote`);
+		
+	}).catch(function(username){
+		alert(`Oh sorry ${username}. You aren't old enough.`)
+	});
+});
+
+
+
+												 
